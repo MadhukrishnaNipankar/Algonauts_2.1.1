d0880ba8a17ebb324bceadb7801c435862c4fdbb
Siddhesh Chaudhari <siddheshchaudhari910@gmail.com>
2024-02-18 15:24:49 +0530

    Major: All_Posts component added

--- Frontend/src/components/AllPosts.jsx
+++ Frontend/src/components/AllPosts.jsx
@@ -18,6 +18,66 @@ const AllPosts = () => {
       "createdAt": "2024-02-18T06:12:14.155Z",
       "__v": 0
     },
+    {
+        "_id": "65d19fbe38181ff7120e8114",
+        "title": "How to continue warsa",
+        "category": "Technology",
+        "content": "<p>This is the content1 of the blog post in HTML format.</p>",
+        "likeCount": 0,
+        "comments": [
+          { id: 1, author: "User1", text: "Great post!" },
+          { id: 2, author: "User2", text: "Interesting insights." },
+        ],
+        "author": "65d0c5bb08ef84d081a5cb6f",
+        "tags": ["sample", "technology", "blog"],
+        "createdAt": "2024-02-18T06:12:14.155Z",
+        "__v": 0
+      },
+      {
+        "_id": "65d19fbe38181ff7120e8114",
+        "title": "How to continue warsa",
+        "category": "Technology",
+        "content": "<p>This is the content1 of the blog post in HTML format.</p>",
+        "likeCount": 0,
+        "comments": [
+          { id: 1, author: "User1", text: "Great post!" },
+          { id: 2, author: "User2", text: "Interesting insights." },
+        ],
+        "author": "65d0c5bb08ef84d081a5cb6f",
+        "tags": ["sample", "technology", "blog"],
+        "createdAt": "2024-02-18T06:12:14.155Z",
+        "__v": 0
+      },
+      {
+        "_id": "65d19fbe38181ff7120e8114",
+        "title": "How to continue warsa",
+        "category": "Technology",
+        "content": "<p>This is the content1 of the blog post in HTML format.</p>",
+        "likeCount": 0,
+        "comments": [
+          { id: 1, author: "User1", text: "Great post!" },
+          { id: 2, author: "User2", text: "Interesting insights." },
+        ],
+        "author": "65d0c5bb08ef84d081a5cb6f",
+        "tags": ["sample", "technology", "blog"],
+        "createdAt": "2024-02-18T06:12:14.155Z",
+        "__v": 0
+      },
+      {
+        "_id": "65d19fbe38181ff7120e8114",
+        "title": "How to continue warsa",
+        "category": "Technology",
+        "content": "<p>This is the content1 of the blog post in HTML format.</p>",
+        "likeCount": 0,
+        "comments": [
+          { id: 1, author: "User1", text: "Great post!" },
+          { id: 2, author: "User2", text: "Interesting insights." },
+        ],
+        "author": "65d0c5bb08ef84d081a5cb6f",
+        "tags": ["sample", "technology", "blog"],
+        "createdAt": "2024-02-18T06:12:14.155Z",
+        "__v": 0
+      },
     // Add more posts as needed
   ];
 
@@ -39,8 +99,11 @@ const styles = {
     padding: "2rem",
     backgroundColor: "#f9f9f9",
     minHeight: "calc(100vh - 56px)",
+    display: "inline-block",
   },
   card: {
+    marginLeft: "5px",
+    marginRight: "5px",
     marginBottom: "20px",
     borderRadius: "15px",
     boxShadow: "0 4px 8px rgba(0, 0, 0, 0.1)",
@@ -53,10 +116,12 @@ const styles = {
   },
   postNumber: {
     fontWeight: "bold",
-    fontSize: "1.2rem",
+    fontSize: "1.5rem",
+    textAlign: "center",
   },
   title: {
     fontSize: "1.2rem",
+    fontWeight: "bold",
   },
   tags: {
     fontSize: "1rem",
@@ -65,12 +130,21 @@ const styles = {
 
 const UserPost = ({ post, index }) => {
   return (
+    
     <Card style={{ ...styles.card, ...styles.cardHover }}>
       <CardBody>
         <div style={styles.postNumber}>Post {index + 1}</div>
+        <hr style={{ margin: "0.5rem 0" }} />
         <div style={styles.title}>{post.title}</div>
+        <div className="mb-3">
+        <hr style={{ margin: "0.5rem 0" }} />
         <div style={styles.tags}>
-          {post.tags.join(", ")}
+          {post.tags.map((tag, index) => (
+            <Badge color="success" className="me-2" key={index}>
+             {post.tags.join(", ")}
+            </Badge>
+          ))}
+        </div>
         </div>
       </CardBody>
     </Card>
