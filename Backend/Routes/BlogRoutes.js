2046c5b79c7e5b006f56a8ca44df31afe003fe8d
Madhukrishna Nipankar <nipankarmadhu@gmail.com>
2024-02-18 12:01:07 +0530

    Blog CRUD API

--- Backend/Routes/BlogRoutes.js
+++ Backend/Routes/BlogRoutes.js
@@ -12,24 +12,24 @@ const {
 const {
   createBlogPost,
   viewBlogPost,
-  //   viewAllMyBlogPosts,
-  //   deleteBlogPost,
-  //   updateBlogPost,
+  viewAllMyBlogPosts,
+  deleteBlogPost,
+  updateBlogPost,
 } = require("../Controllers/UserControllers/BlogController");
 
 //  Create A Blog Post
 router.post("/", protect, createBlogPost);
 
+// View All Blog Posts
+router.get("/all", protect, viewAllMyBlogPosts);
+
 // View Blog Post By ID
 router.get("/:id", protect, viewBlogPost);
 
-// View All My Blogs
-// router.get("/myblogs", protect, viewAllMyBlogPosts);
+// Update
+router.patch("/:id", protect, updateBlogPost);
 
-// // Delete Blog Post
-// router.delete("/:id", protect, deleteBlogPost);
-
-// Update Blog Post
-// router.patch("/:id", protect, updateBlogPost);
+// Delete
+router.delete("/:id", protect, deleteBlogPost);
 
 module.exports = router;
